bd90cfd96950959f535c24bdc877161ccded31d6
Le <lem19@student.douglascollege.ca>
2023-08-16 12:11:43 -0700

    commit change collection name

--- models/model.js
+++ models/model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
-    description: { type: String, required: true }
+    description: { type: String }
 });
 
-const Book = mongoose.model('Book', bookSchema);
+const Book = mongoose.model('300365849-Quan', bookSchema);
 
 module.exports = Book;
